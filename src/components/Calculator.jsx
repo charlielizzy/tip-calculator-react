9e191371090d9836ab1d0874ac443a9ac835e8cf
Charlotte Walker <charlielizzy@me.com>
2022-05-28 10:29:48 +0100

    some styling, functionality broken

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -43,16 +43,14 @@ console.log("tipPerPerson", tipPerPerson);
 console.log("totalPerPerson", totalPerPerson);
 
 return (
-    <div>
-        {/* <body> */}
-    <h2 className="text-red-900">Spli</h2>
-    <br />
-    <h2>Tter</h2>
-    <div class="container">
-      <div>
+    <div className="flex justify-center flex-col">
+    <h2 className="flex justify-center uppercase tracking-widest">Spli</h2>
+    <h2 className="flex justify-center uppercase tracking-widest">Tter</h2>
+    <div className="container bg-white flex flex-row justify-center w-[920px] min-h-fit rounded-3xl">
+      <div className="left-side w-[379px] h-[388px] m-[48px] content-around">
         <div>
           <p>Bill</p>
-          <input type="number" className="border-2 border-blue-500" value={bill} onChange={(e) => setBill(e.target.value)}/>
+          <input type="number" className="mb-8 border-2 border-blue-500" value={bill} onChange={(e) => setBill(e.target.value)}/>
         </div>
         <div>
           <p>Select tip %</p>
@@ -102,22 +100,21 @@ return (
             onClick={handleCheck}
           /><label for="50">50%</label>
           <label for="custom">Custom amount</label>
-           <input type="number" id="custom" min="0" max="100" className="border-2 border-blue-500" onChange={(e) => setTipPercent(e.target.value / 100)} value={inputChecked ? "" : tipPercent * 100} onFocus={() =>  setInputChecked(false)}/>
+           <input type="number" id="custom" min="0" max="100" className="mb-8 border-2 border-blue-500" onChange={(e) => setTipPercent(e.target.value / 100)} value={inputChecked ? "" : tipPercent * 100} onFocus={() =>  setInputChecked(false)}/>
         </div>
         <div id="number-of-people">
-          <p className="text-red-900">Number of people</p>
+          <p className="">Number of people</p>
           <input type="number" className="border-2 border-blue-500" value={people} min="1" max="8" onChange={(e) => setPeople(e.target.value)}/>
         </div>
       </div>
-      <div class="right-side">
-        <div><p>Tip amount /person {tipPerPerson ? `£${tipPerPerson.toFixed(2)}` : null}</p></div>
+      <div class="right-side flex flex-col justify-between bg-[#00474b] w-[333px] h-[337px] rounded-2xl m-[32px] p-[40px] text-white">
+        <div><p>Tip amount /person</p><p className="text-white">{tipPerPerson ? `£${tipPerPerson.toFixed(2)}` : null}</p></div>
         <div><p>Bill amount /person {totalPerPerson ? `£${totalPerPerson.toFixed(2)}` : null}</p></div>
         <div class="reset-button" >
-          <button type="button" onClick={handleReset} >Reset</button>
+          <button type="button" className="bg-[#26c0ab] text-[#00474b] w-[333px] h-[48px] rounded-sm" onClick={handleReset} >Reset</button>
         </div>
       </div>
     </div>
-  {/* </body> */}
     </div>
 )
 }
